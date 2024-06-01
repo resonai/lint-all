d6314bc59ee3c1599189b7820e4eb02dc6636786
shirpeled <shirpeled@gmail.com>
2024-06-01 19:34:43 +0300

    Moving files around

--- setup.py
+++ setup.py
@@ -2,7 +2,23 @@ from setuptools import setup
 
 setup(
     entry_points={
-        "console_scripts": ["mybinary=lintall.lint_all:main"],
+        "console_scripts": ["lint_all=lint_all.lint_all:parse_args_and_run"],
     },
-    name="lintall",
+    install_requires=[
+        "cpplint",
+        "gitdb",
+        "GitPython",
+        "mypy",
+        "pyenchant",
+        "pylint",
+        "types-mock",
+        "types-protobuf",
+        "types-redis",
+        "types-requests",
+        "types-setuptools",
+        "types-six",
+        "types-pyyaml",
+        "pyyaml",
+    ],
+    name="lint_all",
 )
